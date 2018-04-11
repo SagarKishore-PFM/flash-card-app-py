5062097488834f6fc12741233465e73d02f3b765
Sagar Kishore <sagarkishore.ckl@gmail.com>
2018-04-11 15:08:12 +0530

    First Commit

---
+++ word.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Saturday, March 31st 2018
+
+@author: sagar
+"""
+
+
+class Word:
+    """
+    Word Class that takes in the word name as mandatory parameter.
+    """
+
+    def __init__(self, name, *args, **kwargs):
+        self.name = name.lower()
+        self.definitions = dict()
+        self.audio = ''
+        self.synant = dict()
+
+    def __str__(self):
+        return f"""\
+        Word Obeject - {self.name}
+        Audio URL - {self.audio}
+        Definitions - {self.definitions}
+        Synonyms and Antonyms - {self.synant}"""
+
+    def __repr____(self):
+        return f'Word Object - {self.name}'
