324e1949a41b9799c7f2bdd3d05dba8a5fc37150
Sagar Kishore <sagarkishore.ckl@gmail.com>
2018-04-17 20:04:17 +0530

    Changed the ViewStackPopup to include ListView Logic, TODO, added CreateStack base logic.

--- stacklistscreen.py
+++ stacklistscreen.py
@@ -15,13 +15,14 @@ from kivy.uix.popup import Popup
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.properties import BooleanProperty
-
 from testdatabase import generate_test_database
 from compound_selection import SelectableLayout
+from kivy.uix.listview import ListItemButton, ListView
+from kivy.adapters.listadapter import ListAdapter
 
 # ChangeLog:
 
-# Version 1.2.0:
+# Version 0.2.0:
 
 # The new UI design allows for ease of data manipulation and access.
 # This includes 4 buttons surrounding the List of stacks.
@@ -34,25 +35,38 @@ from compound_selection import SelectableLayout
 
 # Add popups for all the functionalities required.
 # Fix the existing popups or create new ones.
+# Text inputs need to have their rules changed
+# Consider reomving max size for stack object ELSE implement the logic for it
+# Create a word database
+
+# TODO:
+# >> View Stack:
+#     Fix the positions of the widgets and make it pretty
+#     Implementation of flash card via Practice Button in the popup
+
+# >> Edit Stack:
+#     Fix the positions of the widgets and make it pretty
+#     Create the word list view and add a button that take you to a different\
+#         screen or popup that allows you to edit the words of the stack
+#     Logic for the edit word UI and backend
+
+# >> Create Stack:
+#     Fix the positions of the widgets and make it pretty
+#     Make the text input mandatory
+#     Create a button that takes you to a different screen or popup that allows\
+#         you to add words to the stack
+#     Make sure that the stack name is unique
+
+# >> Delete Stack:
+#     Everything
 
 STACK_DATABASE = generate_test_database()
 
+# class AddTestButton(Button):
 
-class StackListApp(App):
-
-    def build(self):
-        return StackListScreen()
-
-
-class EditStackPopup2(Popup):
-    pass
-
-
-class AddTestButton(Button):
-
-    def openpop(self):
-        popup = EditStackPopup2()
-        popup.open()
+#     def openpop(self):
+#         popup = EditStackPopup2()
+#         popup.open()
 
 
 class SelectableGridLayout(SelectableLayout, GridLayout):
@@ -65,6 +79,7 @@ class SelectableGridLayout(SelectableLayout, GridLayout):
         self.selected_nodes_list = self.selected_nodes
         print(self.selected_nodes_list[0].stack_object)
 
+    # For Debugging
     def return_selected_stack(self):
         self.selected_nodes_list = self.selected_nodes
         selected_stack_object = self.selected_nodes_list[0].stack_object
@@ -72,11 +87,7 @@ class SelectableGridLayout(SelectableLayout, GridLayout):
 
     def return_selected_stack_layout(self):
         self.selected_nodes_list = self.selected_nodes
-        try:
-            selected_stack_layout = self.selected_nodes_list[0]
-            return selected_stack_layout
-        except IndexError:
-            return False
+        return self.selected_nodes_list[0]
 
     def on_selected_nodes(self, gird, nodes):
         self.button_disabled = bool(not self.selected_nodes)
@@ -98,6 +109,19 @@ class StackRelativeLayout(SelectableLayout, RelativeLayout):
         self.ids.StackSizeLabel.text = str(self.stack_object.size)
 
 
+class WordListAdapter(ListAdapter):
+
+    word_object_list = ObjectProperty(None)
+
+
+class ViewStackWordViewList(ListView):
+    pass
+
+
+class WordGridLayout(GridLayout):
+    pass
+
+
 class ViewStackPopup(Popup):
 
     stack_name_lbl = ObjectProperty(None)
@@ -110,8 +134,22 @@ class ViewStackPopup(Popup):
         super(ViewStackPopup, self).__init__(*args, **kwargs)
         self.ids.StackNameLbl.text = self.stack_object.name
         self.ids.StackSizeLbl.text = str(self.stack_object.size)
-        self.ids.StackWordsLbl.text = ','.join(str(x.name) for x in self.stack_object.words)
-        self.ids.StackMaxSizeLbl.text = str(self.stack_object.maxsize)
+        # self.ids.StackMaxSizeLbl.text = str(self.stack_object.maxsize)
+        self.word_list_view = self.ids.WordViewList
+        self.generate_word_listview(self.stack_object.words,
+                                    self.word_list_view)
+
+    def generate_word_listview(self, word_object_list, word_list_view):
+
+        list_adapter = WordListAdapter(data=word_object_list,
+                                       args_converter=lambda row_index, rec: {'text': rec.name,
+                                                                              'size_hint_y': None,
+                                                                              'height': 25},
+                                       selection_mode="single",
+                                       allow_empty_selection=False,
+                                       cls=ListItemButton,
+                                       word_object_list=word_object_list)
+        word_list_view.adapter = list_adapter
 
 
 class ViewStackPopupButton(Button):
@@ -122,6 +160,17 @@ class ViewStackPopupButton(Button):
     def open_popup(self, selected_stack_relative_layout):
         selected_stack_object = selected_stack_relative_layout.stack_object
         popup = ViewStackPopup(selected_stack_object)
+        popup.open()
+
+
+class CreateStackPopup(Popup):
+    pass
+
+
+class CreataStackPopupButton(Button):
+
+    def open_popup(self):
+        popup = CreateStackPopup()
         popup.open()
 
 
@@ -179,7 +228,12 @@ class StackListScreen(Screen):
     def add_stack_widget(self, stack_object):
         SRL = StackRelativeLayout(stack_object)
         self.ids.SelectableGridLayout.add_widget(SRL)
-    pass
+
+
+class StackListApp(App):
+
+    def build(self):
+        return StackListScreen()
 
 
 if __name__ == '__main__':
