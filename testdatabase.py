a852f00f6ef291066afa69ca096d8279a6831b4f
Sagar Kishore <sagarkishore.ckl@gmail.com>
2018-04-18 17:26:08 +0530

    Added a new method that converts json to word objects and renamed the equivalent method for stack objects from generate_test_database to generate_test_stack_database

--- testdatabase.py
+++ testdatabase.py
@@ -10,7 +10,7 @@ from word import Word
 from stack import Stack
 
 
-def fromjson(json_data: list) -> list:
+def stackobjectfromjson(json_data: list) -> list:
     """Converts a list of stack objects represented in json to python object"""
     # assert(type(json_list) is list), 'Function requires a list'
     object_list = []
@@ -27,23 +27,50 @@ def fromjson(json_data: list) -> list:
     return object_list
 
 
-def generate_test_database(file: str=None) -> str:
+def wordobjectfromjson(json_data: list) -> list:
+    """Converts a list of word objects represented in json to python object"""
+
+    object_list = []
+    for entry in json_data:
+        word_object = Word(entry['name'])
+        word_object.definitions = entry['definitions']
+        word_object.audio = entry['audio']
+        word_object.synant = entry['synant']
+        object_list.append(word_object)
+    return object_list
+
+
+def generate_test_word_database(file: str=None) -> list:
     """
     Function to generate or fetch database from a file. If no file is given,
     it will automatically use the test database.
     """
     if(file is None):
-        file = r'E:\Projects\Flash Card App\testdb.json'
+        file = r'E:\Projects\Flash Card App\test_word_db.json'
     with open(file, 'r') as f:
         json_data = json.load(f)
-    database = fromjson(json_data)
+    database = wordobjectfromjson(json_data)
+    return database
+
+
+def generate_test_stack_database(file: str=None) -> list:
+    """
+    Function to generate or fetch database from a file. If no file is given,
+    it will automatically use the test database.
+    """
+    if(file is None):
+        file = r'E:\Projects\Flash Card App\test_stack_db.json'
+    with open(file, 'r') as f:
+        json_data = json.load(f)
+    database = stackobjectfromjson(json_data)
     return database
 
 
 def main():
-    file = r'E:\Projects\Flash Card App\testdb.json'
-    database = generate_test_database(file)
-    print(database)
+    word_database = generate_test_word_database()
+    stack_database = generate_test_stack_database()
+    print(len(word_database))
+    print(len(stack_database))
 
 
 if __name__ == '__main__':
