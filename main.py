5c95e7515def954a26d366089e54d86be3dd4e19
Sagar Kishore <sagarkishore.ckl@gmail.com>
2018-06-21 17:32:09 +0530

    Changed App configuration to start in a Fullscreen Borderless Window. Also added a check to see if the temp folder exists and create it if it doesn't.

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from databasescreen import DataBaseApp
+from kivy.config import Config
+import os
+
+Config.set('kivy', 'desktop', 1)
+Config.set('kivy', 'exit_on_escape', 1)
+Config.set('graphics', 'resizable', 1)
+Config.set('graphics', 'window_state', 'maximized')
+Config.set('graphics', 'borderless', 1)
+Config.set('graphics', 'fullscreen', 'auto')
+Config.write()
+
+
+def folder_init():
+    cwd = os.getcwd()
+    if not (os.path.exists(os.path.join(cwd, 'temp'))):
+        temp_path = os.path.join(cwd, 'temp')
+        os.mkdir(temp_path)
+
 
 if __name__ == '__main__':
+    folder_init()
     DataBaseApp().run()
