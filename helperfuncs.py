9f3cec6f9a0a5737d61bbcca99954c5ece57c956
Sagar Kishore <sagarkishore.ckl@gmail.com>
2019-05-18 12:58:45 +0530

    Better word description and short meaning

--- helperfuncs.py
+++ helperfuncs.py
@@ -59,42 +59,67 @@ def delete_temp():
 
 def word_description(word):
 
-    header_string = f"""[b][size=40]{word.name}[/size][/b]
+    header_string = f"""[b][size=40]{word.name.title()}[/size][/b]
 
     """
+
     defex_string = ""
     azlower = list(string.ascii_lowercase)
     for i, x in enumerate(word.definitions):
         definitions = '.\n'.join(word.definitions[x]['definition'])
-        definition_string = f"""[size=26][b]({'i'*(i+1)}) {x}: [/b][/size][size=22]{definitions}[/size]
+        definition_string = f"""[size=22][b]({'i'*(i+1)}) {x}: [/b][/size][size=22]{definitions}[/size]
 
     """
-        examples_core = ""
-        for j, ex in enumerate(word.definitions[x]['examples']):
-            examples_core = examples_core + f"({azlower[j]})  "\
-                + ex + '\n' + '            '
-        examples_string = f"""[size=20][b]Examples: [/b][/size]
+        if word.definitions[x]['examples'] != []:
+            examples_core = ""
+            for j, ex in enumerate(word.definitions[x]['examples']):
+                examples_core = examples_core + f"({azlower[j]})  " + ex + '\n' + '            '
+            examples_string = f"""[size=20][b]Example(s): [/b][/size]
 
-            [size=18][i]{examples_core}[/size][/i]
+            [size=20][i]{examples_core}[/size][/i]
     """
+        else:
+            examples_string = ""
         defex_string = defex_string + definition_string + examples_string
+
+    synant_header_string = f"""[b][u][size=28]Synonyms and Antonyms:[/size][/u][/b]
+
+    """
 
     synant_string = ""
 
+    if word.synant != {}:
+        synant_string = synant_string + synant_header_string
+
     for i, x in enumerate(word.synant):
-        # try:
-        syns = ', '.join(word.synant[x]['synonyms'])
-        syn_string = f"""[size=26][b]({'i'*(i+1)}) {x}:[/b][/size]
+        if word.synant[x]['synonyms'] != []:
+            syns = ', '.join(word.synant[x]['synonyms'])
+            syn_string = f"""[size=22][b]({'i'*(i+1)}) {x}:[/b][/size]
 
-        [b][size=20]Synonyms:[/b][/size]  [i][size=22]{syns}[/size][/i]
-
-    """
-        ants = ', '.join(word.synant[x]['antonyms'])
-        ant_string = f"""\
-    [size=20][b]Antonyms: [/b][i][size=22]{ants}[/size][/i][/size]
+        [b][size=22]Synonyms:[/b][/size]  [i][size=22]{syns}[/size][/i]
 
     """
+        else:
+            syn_string = ""
+
+        if word.synant[x]['antonyms'] != []:
+            ants = ', '.join(word.synant[x]['antonyms'])
+            ant_string = f"""\
+    [size=22][b]Antonyms: [/b][i][size=22]{ants}[/size][/i][/size]
+
+    """
+
+        else:
+            ant_string = ""
+
         synant_string = synant_string + syn_string + ant_string
+
+    main_string = ""
+
+    if defex_string == synant_string == "":
+        oopsie_string = f"""[/b][i][size=22]Please try a different form of the word.[/size][/i][/size]"""
+        main_string = header_string + oopsie_string
+        return main_string
 
     main_string = header_string + defex_string + synant_string
     return main_string
@@ -102,10 +127,13 @@ def word_description(word):
 
 def short_meaning(word):
     short_meaning_string = ""
-    for i, x in enumerate(word.definitions):
-        definitions = '.\n'.join(word.definitions[x]['definition'])
-        definition_string = f"""[size=18][b]({'i'*(i+1)}) {x}: [/size][size=16]{definitions}[/size][/b]
+    if word.definitions != {}:
+        for i, x in enumerate(word.definitions):
+            definitions = '.\n'.join(word.definitions[x]['definition'])
+            definition_string = f"""[size=18][b]({'i'*(i+1)}) {x}: [/size][size=16]{definitions}[/size][/b]
 """
         short_meaning_string = short_meaning_string + definition_string
 
+    else:
+        short_meaning_string = "[size=18][b]N/A[/size][/b]"
     return short_meaning_string
